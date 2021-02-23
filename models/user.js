f41c43a33795c78d391d5c0e81e99a18b1b1d208
Igor Znamensky <igor.znamensky@gmail.com>
2021-02-23 20:18:45 +0300

    Блок 4. Практика: База данных MongoDB, Done: Урок 7-8 Next: Урок 9

--- models/user.js
+++ models/user.js
@@ -28,4 +28,37 @@ const userSchema = new Schema({
     }
 })
 
+userSchema.methods.addToCart = function (course) {
+    // concat() -- вернут новый (копию) массив
+    // es6 использовать троиточие или оператор развертки [...this.cart.items]
+    const items = [...this.cart.items]
+    const idx = items.findIndex(c => {
+        return c.courseId.toString() === course._id.toString()
+    })
+
+    // если инедекс, то увеличить на 1
+    if (idx >= 0) {
+        items[idx].count++
+    } else {
+        // иначе добавь объект с id and count
+        items.push({
+            courseId: course._id.toString(),
+            count: 1
+        })
+    }
+
+    // длинный вариант
+    // const newCart = model({items: items})
+    // this.cart = newCart
+
+    // короткий
+    // this.cart = {items: items}
+
+    // что эквивалент, т.к. в случае совпадения ключа и значения, можно записать
+    // только ключ
+    this.cart = {items}
+
+    return this.save()
+}
+
 module.exports = model('User', userSchema)
